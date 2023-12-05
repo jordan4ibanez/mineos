ff30b85da7403b14a13bafeb7a10de24b4622a62
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-05 12:55:24 -0500

    Well I didn't even know this was possible

--- mods/mineos/programs.ts
+++ mods/mineos/programs.ts
@@ -1,20 +1,33 @@
 namespace mineos {
 
-  export interface Program {
-    execute(): void;
+  let programFoundation: {[id: string] : typeof Program} = {}
+
+  // Oh my god it's java in minetest. What have I doneeeeeeeeeee
+  export interface ProgramInterface {
+    execute(delta: number): void;
   }
 
-  export class ProgramBase {
-    system: System | null = null
-    constructor(system: System) {
+  export class Program implements ProgramInterface {
+    system: System
+    renderer: Renderer
+    constructor(system: System, renderer: Renderer) {
       this.system = system
+      this.renderer = renderer
+    }
+    execute(delta: number): void {
+      
     }
   }
 
-  let programFoundation: {[id: string] : Program} = {}
-
+  programFoundation.bootProcedure = class BootProcedure extends Program {
+    memoryTest = 0
+    execute(delta: number): void {      
+      this.memoryTest += delta
+      print("booting! " + this.memoryTest)
+    }
+  }
   
-  function injectStates(): {[id: string] : Program} {
+  export function grabFoundationalPrograms(): {[id: string] : typeof Program} {
     return programFoundation    
   }
 }
