2fe5df3d2a7420e2a6781979ae5a78ad1b891d93
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-05 10:53:59 -0500

    renderer

--- mods/mineos/renderer.lua
+++ mods/mineos/renderer.lua
@@ -1,4 +1,24 @@
+-- Lua Library inline imports
+local function __TS__Class(self)
+    local c = {prototype = {}}
+    c.prototype.__index = c.prototype
+    c.prototype.constructor = c
+    return c
+end
+
+local function __TS__New(target, ...)
+    local instance = setmetatable({}, target.prototype)
+    instance:____constructor(...)
+    return instance
+end
+-- End of Lua Library inline imports
 mineos = mineos or ({})
 do
+    local Renderer = __TS__Class()
+    Renderer.name = "Renderer"
+    function Renderer.prototype.____constructor(self)
+        print("hello I am a renderer")
+    end
+    __TS__New(Renderer)
     print("renderer loaded.")
 end
--- mods/mineos/renderer.ts
+++ mods/mineos/renderer.ts
@@ -1,4 +1,12 @@
 namespace mineos {
-  
+  class Renderer {
+    constructor() {
+      print("hello I am a renderer")
+    }
+  }
+
+  // Now move it into namespace.
+  export const renderer = new Renderer();
+
   print("renderer loaded.")
 }
