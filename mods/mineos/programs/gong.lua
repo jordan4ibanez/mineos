7be6e1c6bd45a8b5ec8a8897beab0aa56dabdebe
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-12-14 17:36:14 -0500

    Yeah that definitely looks like a game

--- mods/mineos/programs/gong.lua
+++ mods/mineos/programs/gong.lua
@@ -30,14 +30,204 @@ local function __TS__ClassExtends(target, base)
         target.prototype.__tostring = base.prototype.__tostring
     end
 end
+
+local function __TS__New(target, ...)
+    local instance = setmetatable({}, target.prototype)
+    instance:____constructor(...)
+    return instance
+end
 -- End of Lua Library inline imports
 mineos = mineos or ({})
 do
+    local create = vector.create2d
+    local create3d = vector.create
+    local dir_to_yaw = minetest.dir_to_yaw
+    local yaw_to_dir = minetest.yaw_to_dir
+    local PIXEL_SIZE = 16
+    local function getPixel(dimension)
+        return dimension * PIXEL_SIZE
+    end
+    local function randomDir()
+        local dir3d = yaw_to_dir(math.random() * (math.pi * 2))
+        return create(dir3d.x, dir3d.z)
+    end
+    local MOVEMENT_MULTIPLIER = 500
     local Gong = __TS__Class()
     Gong.name = "Gong"
     __TS__ClassExtends(Gong, mineos.WindowProgram)
-    function Gong.prototype.main(self, delta)
-        print("gongtastic!")
+    function Gong.prototype.____constructor(self, system, renderer, audio, desktop, size)
+        Gong.____super.prototype.____constructor(
+            self,
+            system,
+            renderer,
+            audio,
+            desktop,
+            size
+        )
+        self.loaded = false
+        self.ball = __TS__New(
+            mineos.AABB,
+            create(0, 0),
+            create(
+                getPixel(1),
+                getPixel(1)
+            ),
+            create(0, 0)
+        )
+        self.ballVelocity = create(1, 0)
+        self.playerPaddle = __TS__New(
+            mineos.AABB,
+            create(0, 0),
+            create(
+                getPixel(1),
+                getPixel(4)
+            ),
+            create(0, 0)
+        )
+        self.instance = Gong.currentInstance
+        Gong.currentInstance = Gong.currentInstance + 1
     end
+    function Gong.prototype.move(self)
+        self.renderer:setElementComponentValue(
+            "gong_bg_" .. tostring(self.instance),
+            "offset",
+            self.windowPosition
+        )
+    end
+    function Gong.prototype.getPlayerPaddlePos(self)
+        return create(self.playerPaddle.offset.x + self.windowPosition.x, self.playerPaddle.offset.y + self.windowPosition.y)
+    end
+    function Gong.prototype.getBallPos(self)
+        return create(self.ball.offset.x + self.windowPosition.x, self.ball.offset.y + self.windowPosition.y)
+    end
+    function Gong.prototype.load(self)
+        self.ball.offset.x = self.windowSize.x / 2 - self.ball.size.x / 2
+        self.ball.offset.y = self.windowSize.y / 2 - self.ball.size.y / 2
+        self.playerPaddle.offset.y = self.windowSize.y / 2 - self.playerPaddle.size.y / 2
+        self.renderer:addElement(
+            "gong_bg_" .. tostring(self.instance),
+            {
+                name = "gong_bg_" .. tostring(self.instance),
+                hud_elem_type = HudElementType.image,
+                position = create(0, 0),
+                text = ("pixel.png^[colorize:" .. colors.color(0, 0, 0)) .. ":255",
+                scale = self.windowSize,
+                alignment = create(1, 1),
+                offset = create(
+                    self:getPosX(),
+                    self:getPosY()
+                ),
+                z_index = 1
+            }
+        )
+        self.renderer:addElement(
+            "gong_player_paddle_" .. tostring(self.instance),
+            {
+                name = "gong_player_paddle_" .. tostring(self.instance),
+                hud_elem_type = HudElementType.image,
+                position = create(0, 0),
+                text = ("pixel.png^[colorize:" .. colors.color(100, 100, 100)) .. ":255",
+                scale = self.playerPaddle.size,
+                alignment = create(1, 1),
+                offset = self:getPlayerPaddlePos(),
+                z_index = 2
+            }
+        )
+        self.renderer:addElement(
+            "gong_ball_" .. tostring(self.instance),
+            {
+                name = "gong_ball_" .. tostring(self.instance),
+                hud_elem_type = HudElementType.image,
+                position = create(0, 0),
+                text = ("pixel.png^[colorize:" .. colors.color(100, 100, 100)) .. ":255",
+                scale = self.ball.size,
+                alignment = create(1, 1),
+                offset = self:getPlayerPaddlePos(),
+                z_index = 2
+            }
+        )
+        self:setWindowTitle("Gong")
+        print("Gong loaded!")
+        self.loaded = true
+    end
+    function Gong.prototype.updateScene(self)
+        self.renderer:setElementComponentValue(
+            "gong_player_paddle_" .. tostring(self.instance),
+            "offset",
+            self:getPlayerPaddlePos()
+        )
+        self.renderer:setElementComponentValue(
+            "gong_ball_" .. tostring(self.instance),
+            "offset",
+            self:getBallPos()
+        )
+    end
+    function Gong.prototype.playerControls(self, delta)
+        local up = self.system:isKeyDown("up")
+        local down = self.system:isKeyDown("down")
+        if up then
+            local ____self_playerPaddle_offset_0, ____y_1 = self.playerPaddle.offset, "y"
+            ____self_playerPaddle_offset_0[____y_1] = ____self_playerPaddle_offset_0[____y_1] - delta * MOVEMENT_MULTIPLIER
+            if self.playerPaddle.offset.y <= 0 then
+                self.playerPaddle.offset.y = 0
+            end
+        elseif down then
+            local ____self_playerPaddle_offset_2, ____y_3 = self.playerPaddle.offset, "y"
+            ____self_playerPaddle_offset_2[____y_3] = ____self_playerPaddle_offset_2[____y_3] + delta * MOVEMENT_MULTIPLIER
+            if self.playerPaddle.offset.y >= self.windowSize.y - self.playerPaddle.size.y then
+                self.playerPaddle.offset.y = self.windowSize.y - self.playerPaddle.size.y
+            end
+        end
+    end
+    function Gong.prototype.ballLogic(self, delta)
+        local ____self_ball_offset_4, ____x_5 = self.ball.offset, "x"
+        local ____self_ball_offset_x_6 = ____self_ball_offset_4[____x_5] + self.ballVelocity.x * delta * MOVEMENT_MULTIPLIER
+        ____self_ball_offset_4[____x_5] = ____self_ball_offset_x_6
+        local ____self_ball_offset_7, ____y_8 = self.ball.offset, "y"
+        local ____self_ball_offset_y_9 = ____self_ball_offset_7[____y_8] + self.ballVelocity.y * delta * MOVEMENT_MULTIPLIER
+        ____self_ball_offset_7[____y_8] = ____self_ball_offset_y_9
+        local ballNewPos = create(____self_ball_offset_x_6, ____self_ball_offset_y_9)
+        do
+            local ballCenterY = self.ball.offset.y + self.ball.size.y / 2
+            local paddleCenterY = self.playerPaddle.offset.y + self.playerPaddle.size.y / 2
+            local upperLeftPoint = create(self.ball.offset.x, self.ball.offset.y)
+            local lowerLeftPoint = create(self.ball.offset.x, self.ball.offset.y + self.ball.size.y)
+            if self.playerPaddle:pointWithin(upperLeftPoint) or self.playerPaddle:pointWithin(lowerLeftPoint) then
+                ballNewPos.x = self.playerPaddle.offset.x + self.playerPaddle.size.x
+                local newDir3d = create3d(ballNewPos.x - self.playerPaddle.offset.x, 0, (ballCenterY - paddleCenterY) / 2)
+                local normalizedDir3d = vector.normalize(newDir3d)
+                self.ballVelocity.x = normalizedDir3d.x
+                self.ballVelocity.y = normalizedDir3d.z
+            end
+        end
+        if ballNewPos.x >= self.windowSize.x - self.ball.size.x then
+            ballNewPos.x = self.windowSize.x - self.ball.size.x
+            local ____self_ballVelocity_10, ____x_11 = self.ballVelocity, "x"
+            ____self_ballVelocity_10[____x_11] = ____self_ballVelocity_10[____x_11] * -1
+        elseif ballNewPos.x <= 0 then
+            ballNewPos.x = 0
+            local ____self_ballVelocity_12, ____x_13 = self.ballVelocity, "x"
+            ____self_ballVelocity_12[____x_13] = ____self_ballVelocity_12[____x_13] * -1
+        end
+        if ballNewPos.y >= self.windowSize.y - self.ball.size.y then
+            ballNewPos.y = self.windowSize.y - self.ball.size.y
+            local ____self_ballVelocity_14, ____y_15 = self.ballVelocity, "y"
+            ____self_ballVelocity_14[____y_15] = ____self_ballVelocity_14[____y_15] * -1
+        elseif ballNewPos.y <= 0 then
+            ballNewPos.y = 0
+            local ____self_ballVelocity_16, ____y_17 = self.ballVelocity, "y"
+            ____self_ballVelocity_16[____y_17] = ____self_ballVelocity_16[____y_17] * -1
+        end
+        self.ball.offset = ballNewPos
+    end
+    function Gong.prototype.main(self, delta)
+        if not self.loaded then
+            self:load()
+        end
+        self:playerControls(delta)
+        self:ballLogic(delta)
+        self:updateScene()
+    end
+    Gong.currentInstance = 0
     mineos.DesktopEnvironment:registerProgram(Gong)
 end
--- mods/mineos/programs/gong.ts
+++ mods/mineos/programs/gong.ts
@@ -1,8 +1,207 @@
 namespace mineos {
   // It's not Pong it's Gong!
+  // Probably the simplest game
+  const create = vector.create2d;
+  const create3d = vector.create;
+  const dir_to_yaw = minetest.dir_to_yaw;
+  const yaw_to_dir = minetest.yaw_to_dir;
+
+  const PIXEL_SIZE = 16
+  function getPixel(dimension: number): number {
+    return dimension * PIXEL_SIZE
+  }
+
+  function randomDir(): Vec2 {
+    const dir3d = yaw_to_dir(math.random() * (math.pi * 2))
+    return create(
+      dir3d.x,
+      dir3d.z
+    )    
+  }
+
+  const MOVEMENT_MULTIPLIER = 500
+
   class Gong extends WindowProgram {
+
+    loaded = false
+
+    instance: number
+
+    static currentInstance = 0
+
+    ball = new AABB(
+      create(0,0),
+      create(getPixel(1),getPixel(1)),
+      create(0,0)
+    )
+
+    ballVelocity = create(1,0)//randomDir()
+
+    playerPaddle = new AABB(
+      create(0,0),
+      create(getPixel(1),getPixel(4)),
+      create(0,0)
+    )
+
+    constructor(system: System, renderer: Renderer, audio: AudioController, desktop: DesktopEnvironment, size: Vec2) {
+      super(system, renderer, audio, desktop, size);
+      this.instance = Gong.currentInstance
+      Gong.currentInstance++
+    }
+
+    move(): void {
+      this.renderer.setElementComponentValue("gong_bg_" + this.instance, "offset", this.windowPosition)
+    }
+
+    getPlayerPaddlePos(): Vec2 {
+      return create(
+        this.playerPaddle.offset.x + this.windowPosition.x,
+        this.playerPaddle.offset.y + this.windowPosition.y
+      )
+    }
+
+    getBallPos(): Vec2 {
+      return create(
+        this.ball.offset.x + this.windowPosition.x,
+        this.ball.offset.y + this.windowPosition.y
+      )
+    }
+
+    load(): void {
+
+      // Center the elements
+      this.ball.offset.x = (this.windowSize.x / 2) - (this.ball.size.x / 2)
+      this.ball.offset.y = (this.windowSize.y / 2) - (this.ball.size.y / 2)
+      this.playerPaddle.offset.y = (this.windowSize.y / 2) - (this.playerPaddle.size.y / 2)
+
+
+
+      this.renderer.addElement("gong_bg_" + this.instance, {
+        name: "gong_bg_" + this.instance,
+        hud_elem_type: HudElementType.image,
+        position: create(0,0),
+        text: "pixel.png^[colorize:" + colors.color(0,0,0) + ":255",
+        scale: this.windowSize,
+        alignment: create(1,1),
+        offset: create(
+          this.getPosX(),
+          this.getPosY(),
+        ),
+        z_index: 1
+      })
+
+      this.renderer.addElement("gong_player_paddle_" + this.instance, {
+        name: "gong_player_paddle_" + this.instance,
+        hud_elem_type: HudElementType.image,
+        position: create(0,0),
+        text: "pixel.png^[colorize:" + colors.color(100,100,100) + ":255",
+        scale: this.playerPaddle.size,
+        alignment: create(1,1),
+        offset: this.getPlayerPaddlePos(),
+        z_index: 2
+      })
+
+      this.renderer.addElement("gong_ball_" + this.instance, {
+        name: "gong_ball_" + this.instance,
+        hud_elem_type: HudElementType.image,
+        position: create(0,0),
+        text: "pixel.png^[colorize:" + colors.color(100,100,100) + ":255",
+        scale: this.ball.size,
+        alignment: create(1,1),
+        offset: this.getPlayerPaddlePos(),
+        z_index: 2
+      })
+
+
+      this.setWindowTitle("Gong")
+
+      print("Gong loaded!")
+      this.loaded = true
+    }
+
+    updateScene(): void {
+      this.renderer.setElementComponentValue("gong_player_paddle_" + this.instance, "offset", this.getPlayerPaddlePos())
+      this.renderer.setElementComponentValue("gong_ball_" + this.instance, "offset", this.getBallPos())
+    }
+
+    playerControls(delta: number): void {
+      const up = this.system.isKeyDown("up")
+      const down = this.system.isKeyDown("down")
+      if (up) {
+        this.playerPaddle.offset.y -= delta * MOVEMENT_MULTIPLIER
+        if (this.playerPaddle.offset.y <= 0) {
+          this.playerPaddle.offset.y = 0
+        }
+      } else if (down) {
+        this.playerPaddle.offset.y += delta * MOVEMENT_MULTIPLIER
+        if (this.playerPaddle.offset.y >= this.windowSize.y - this.playerPaddle.size.y) {
+          this.playerPaddle.offset.y = this.windowSize.y - this.playerPaddle.size.y
+        }
+      }
+    }
+
+    ballLogic(delta: number): void {
+      const ballNewPos = create(
+        this.ball.offset.x += this.ballVelocity.x * delta * MOVEMENT_MULTIPLIER,
+        this.ball.offset.y += this.ballVelocity.y * delta * MOVEMENT_MULTIPLIER
+      )
+
+      { // Very cheap collision detection
+        const ballCenterY = this.ball.offset.y + (this.ball.size.y / 2)
+
+        const paddleCenterY = this.playerPaddle.offset.y + (this.playerPaddle.size.y / 2)
+        
+        const upperLeftPoint = create(
+          this.ball.offset.x,
+          this.ball.offset.y
+        )
+
+        const lowerLeftPoint = create(
+          this.ball.offset.x,
+          this.ball.offset.y + this.ball.size.y
+        )
+
+        if (this.playerPaddle.pointWithin(upperLeftPoint) || this.playerPaddle.pointWithin(lowerLeftPoint)) {
+          ballNewPos.x = this.playerPaddle.offset.x + this.playerPaddle.size.x
+
+          // This is surprisingly accurate.
+          const newDir3d = create3d(
+            ballNewPos.x - this.playerPaddle.offset.x,
+            0,
+            (ballCenterY - paddleCenterY) / 2
+          )
+          const normalizedDir3d = vector.normalize(newDir3d)
+
+          this.ballVelocity.x = normalizedDir3d.x
+          this.ballVelocity.y = normalizedDir3d.z
+        }
+      }
+
+      //todo: This is also the points system
+      if (ballNewPos.x >= this.windowSize.x - this.ball.size.x) {
+        ballNewPos.x = this.windowSize.x - this.ball.size.x
+        this.ballVelocity.x *= -1
+      } else if (ballNewPos.x <= 0) {
+        ballNewPos.x = 0
+        this.ballVelocity.x *= -1
+      }
+
+      if (ballNewPos.y >= this.windowSize.y - this.ball.size.y) {
+        ballNewPos.y = this.windowSize.y - this.ball.size.y
+        this.ballVelocity.y *= -1
+      } else if (ballNewPos.y <= 0) {
+        ballNewPos.y = 0
+        this.ballVelocity.y *= -1
+      }
+
+      this.ball.offset = ballNewPos
+    }
+
     main(delta: number): void {
-      print("gongtastic!")
+      if (!this.loaded) this.load()
+      this.playerControls(delta)
+      this.ballLogic(delta)
+      this.updateScene()
     }
   }
   DesktopEnvironment.registerProgram(Gong)
